6e055298eed9fba551c8d0e5d6db9be58b7bb594
Gianluca Cannata <gcannata23gmail.com>
2024-06-13 17:08:08 +0200

    feat: add FindIPP.cmake

--- aws-lambda-intel-ipp/main.cpp
+++ aws-lambda-intel-ipp/main.cpp
@@ -2,16 +2,26 @@
 #include <aws/core/utils/HashingUtils.h>
 #include <aws/lambda-runtime/runtime.h>
 
-#include <ipp/ipp.h>
+#include <ipp.h>
 
 using namespace aws::lambda_runtime;
 
 static invocation_response handle_request(invocation_request const& req)
 {
+    using namespace Aws::Utils::Json;
 
+    const IppLibraryVersion *lib = ippGetLibVersion();
+    
+    JsonValue response;
+    response.WithString("name", lib->Name);
+    response.WithString("version", lib->Version);
+
+    return invocation_response::success(response.View().WriteReadable(false), "application/json; charset=utf-8");
 }
 
 int main()
 {
+    ippInit();
+    
     run_handler(handle_request);
 }
