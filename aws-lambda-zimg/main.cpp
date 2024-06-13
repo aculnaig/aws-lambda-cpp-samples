64d1b29fb52761f1e0bc5b335dac47dbea19428a
Gianluca Cannata <gcannata23gmail.com>
2024-06-13 09:16:25 +0200

    Add aws-lambda-zimg sample

---
+++ aws-lambda-zimg/main.cpp
@@ -0,0 +1,108 @@
+#include <aws/core/Aws.h>
+#include <aws/core/utils/HashingUtils.h>
+#include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/lambda-runtime/runtime.h>
+
+#include <zimg.h>
+
+using namespace aws::lambda_runtime;
+
+static invocation_response my_handler(invocation_request const& req)
+{
+    using namespace Aws::Utils::Json;
+    using namespace Aws::Utils;
+
+    // Decode base64 image
+    JsonValue json(req.payload);
+    if (!json.WasParseSuccessful()) {
+        return invocation_response::failure("Failed to parse input JSON", "InvalidJSON");
+    }
+
+    auto v = json.View();
+
+    if (!v.ValueExists("body")) {
+        return invocation_response::failure("Request body is not a string", "InvalidBody"); 
+    }
+
+    auto body = HashingUtils::Base64Decode(v.GetString("body"));
+    unsigned char *body_buffer = (unsigned char *) body.GetUnderlyingData();
+    size_t body_len = body.GetLength();
+
+    unsigned src_w = 1920;
+    unsigned src_h = 1080;
+
+    unsigned dst_w = 1920 / 2;
+    unsigned dst_h = 1080 / 2;
+    
+    unsigned body_buffer_resize_len = dst_w * dst_h * 3;
+    unsigned char body_buffer_resize[body_buffer_resize_len];
+
+    // Resize the image by half with zimg library
+    zimg_filter_graph *graph = 0;
+	zimg_image_buffer_const src_buf = { ZIMG_API_VERSION };
+	zimg_image_buffer dst_buf = { ZIMG_API_VERSION };
+	zimg_image_format src_format;
+	zimg_image_format dst_format;
+	size_t tmp_size;
+	void *tmp = 0;
+
+    src_format.width = src_w;
+	src_format.height = src_h;
+	src_format.pixel_type = ZIMG_PIXEL_BYTE;   
+
+    src_format.subsample_w = 1;
+	src_format.subsample_h = 1;
+
+    src_format.color_family = ZIMG_COLOR_YUV;
+
+    dst_format.width = dst_w;
+	dst_format.height = dst_h;
+	dst_format.pixel_type = ZIMG_PIXEL_BYTE;
+
+	dst_format.subsample_w = 1;
+	dst_format.subsample_h = 1;
+
+	dst_format.color_family = ZIMG_COLOR_YUV;
+
+    zimg_filter_graph_build(&src_format, &dst_format, 0);
+
+    zimg_filter_graph_get_tmp_size(graph, &tmp_size);
+
+    tmp = malloc(tmp_size);
+
+    src_buf.plane[0].data = body_buffer;
+    src_buf.plane[0].stride = src_w;
+    src_buf.plane[0].mask = ZIMG_BUFFER_MAX;
+    
+    dst_buf.plane[0].data = body_buffer_resize;
+    dst_buf.plane[0].stride = dst_w;
+    dst_buf.plane[0].mask = ZIMG_BUFFER_MAX;
+
+    zimg_filter_graph_process(graph, &src_buf, &dst_buf, tmp, 0, 0, 0, 0);
+
+    zimg_filter_graph_free(graph);
+    free(tmp);
+
+    Aws::Utils::ByteBuffer body_res(body_buffer_resize, body_buffer_resize_len);
+
+    auto image_encoded = HashingUtils::Base64Encode(body_res);
+
+    JsonValue res;
+    res.WithString("body", image_encoded);
+
+    return invocation_response::success(res.View().WriteCompact(false), "application/json; charset=utf-8");
+}
+
+int main()
+{
+    using namespace Aws;
+
+    SDKOptions options;
+    InitAPI(options);
+    {
+        run_handler(my_handler);
+    }
+    ShutdownAPI(options);
+
+    return 0;
+}
