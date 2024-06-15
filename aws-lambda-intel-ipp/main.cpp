9f265f5289e9108a2354844bf838e9b61a2ca18b
Gianluca Cannata <gcannata23gmail.com>
2024-06-15 13:38:04 +0200

    Update aws-lambda-ipp

--- aws-lambda-intel-ipp/main.cpp
+++ aws-lambda-intel-ipp/main.cpp
@@ -17,12 +17,34 @@ static invocation_response handle_request(invocation_request const& req)
         if (payload.View().GetBool("isBase64Encoded") == false)
             return invocation_response::failure("The file is not a JPEG", "InvalidJPEG");
 
+    auto jpegImage = HashingUtils::Base64Decode(payload.View().GetString("body")); 
+
+    Ipp8u *srcIppImage = ippsMalloc_8u(jpegImage.GetLength());
+    ippsCopy_8u(srcIppImage, jpegImage.GetUnderlyingData(), jpegImage.GetLength());
+
+    Ipp8u *dstIppImage = ippsMalloc_8u((1920 / 2) * (1080 / 2) * 3);
+    int srcStep = 1920 * 3;
+    int dstStep = (1920 / 2) * 3;
+
+    IppiSize srcSize = { 1920, 1080 };
+    IppiRect srcRect = { 0, 0, 1920, 1080 };
+    IppiSize dstSize = { (1920 / 2), (1080 / 2) };
+    IppiPoint dstOffset = { 0, 0 };
+
+    ippiResizeCubic_8u_C3R(srcIppImage, srcStep, dstIppImage, dstStep, dstOffset, dstSize, ippBorderRepl, 0, NULL, NULL);
+
+    ByteBuffer dstJpegImageBuffer(dstIppImage, (1920 / 2) * (1080 / 2) * 3);
+    auto dstJpegImage = HashingUtils::Base64Encode(dstJpegImageBuffer);
+
     JsonValue response;
     response.WithInteger("statusCode", 200);
     response.WithBool("isBase64Encoded", true);
     response.WithString("body", payload.View().GetString("body"));
 
-    return invocation_response::success(response.View().WriteReadable(false), "image/jpg");
+    ippsFree(srcIppImage);
+    ippsFree(dstIppImage);
+
+    return invocation_response::success(response.View().WriteCompact(false), "image/jpg");
 }
 
 int main()
